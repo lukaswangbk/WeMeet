839997181f083ffaf33c0a7ee4526ed74176a1b1
Lukas Wang <lukaswangbk@gmail.com>
2022-05-13 16:32:56 -0400

    add project

---
+++ WeMeet-main/js/apiGateway-js-sdk/apigClient.js
@@ -0,0 +1,303 @@
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+var apigClientFactory = {};
+apigClientFactory.newClient = function (config) {
+    var apigClient = { };
+    if(config === undefined) {
+        config = {
+            accessKey: '',
+            secretKey: '',
+            sessionToken: '',
+            region: '',
+            apiKey: undefined,
+            defaultContentType: 'application/json',
+            defaultAcceptType: 'application/json'
+        };
+    }
+    if(config.accessKey === undefined) {
+        config.accessKey = '';
+    }
+    if(config.secretKey === undefined) {
+        config.secretKey = '';
+    }
+    if(config.apiKey === undefined) {
+        config.apiKey = '';
+    }
+    if(config.sessionToken === undefined) {
+        config.sessionToken = '';
+    }
+    if(config.region === undefined) {
+        config.region = 'us-east-1';
+    }
+    //If defaultContentType is not defined then default to application/json
+    if(config.defaultContentType === undefined) {
+        config.defaultContentType = 'application/json';
+    }
+    //If defaultAcceptType is not defined then default to application/json
+    if(config.defaultAcceptType === undefined) {
+        config.defaultAcceptType = 'application/json';
+    }
+
+    
+    // extract endpoint and path from url
+    var invokeUrl = 'https://giud5rrpki.execute-api.us-east-1.amazonaws.com/finalproject';
+    var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
+    var pathComponent = invokeUrl.substring(endpoint.length);
+
+    var sigV4ClientConfig = {
+        accessKey: config.accessKey,
+        secretKey: config.secretKey,
+        sessionToken: config.sessionToken,
+        serviceName: 'execute-api',
+        region: config.region,
+        endpoint: endpoint,
+        defaultContentType: config.defaultContentType,
+        defaultAcceptType: config.defaultAcceptType
+    };
+
+    var authType = 'NONE';
+    if (sigV4ClientConfig.accessKey !== undefined && sigV4ClientConfig.accessKey !== '' && sigV4ClientConfig.secretKey !== undefined && sigV4ClientConfig.secretKey !== '') {
+        authType = 'AWS_IAM';
+    }
+
+    var simpleHttpClientConfig = {
+        endpoint: endpoint,
+        defaultContentType: config.defaultContentType,
+        defaultAcceptType: config.defaultAcceptType
+    };
+
+    var apiGatewayClient = apiGateway.core.apiGatewayClientFactory.newClient(simpleHttpClientConfig, sigV4ClientConfig);
+    
+    
+    
+    apigClient.createmeetingPost = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['body'], ['body']);
+        
+        var createmeetingPostRequest = {
+            verb: 'post'.toUpperCase(),
+            path: pathComponent + uritemplate('/createmeeting').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(createmeetingPostRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.createmeetingOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var createmeetingOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/createmeeting').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(createmeetingOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.editprofileGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['uFN', 'uID'], ['body']);
+        
+        var editprofileGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/editprofile').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, ['uFN', 'uID']),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(editprofileGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.editprofileOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var editprofileOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/editprofile').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(editprofileOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.findusermeetingsGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['uID'], ['body']);
+        
+        var findusermeetingsGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/findusermeetings').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, ['uID']),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(findusermeetingsGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.findusermeetingsOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var findusermeetingsOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/findusermeetings').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(findusermeetingsOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.getrecommendGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['mID'], ['body']);
+        
+        var getrecommendGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/getrecommend').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, ['mID']),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(getrecommendGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.getrecommendOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var getrecommendOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/getrecommend').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(getrecommendOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.loginstatusGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['uPSWD', 'uID'], ['body']);
+        
+        var loginstatusGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/loginstatus').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, ['uPSWD', 'uID']),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(loginstatusGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.loginstatusOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var loginstatusOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/loginstatus').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(loginstatusOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.votemeetingPost = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['body'], ['body']);
+        
+        var votemeetingPostRequest = {
+            verb: 'post'.toUpperCase(),
+            path: pathComponent + uritemplate('/votemeeting').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(votemeetingPostRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.votemeetingOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var votemeetingOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/votemeeting').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(votemeetingOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+
+    return apigClient;
+};
---
+++ WeMeet-main/js/index.js
@@ -0,0 +1,366 @@
+const today = new Date();
+
+var apigClient = apigClientFactory.newClient({
+    region: 'us-east-1', // OPTIONAL: The region where the API is deployed, by default this parameter is set to us-east-1
+    apiKey: 'WFY7aanS24uJZxz05XHo4o1wN7zSszFaOGeg7Ok5'
+});
+
+
+
+function userLogin(){
+    var user_login_id = document.getElementById('user_login_id');
+    console.log(user_login_id)
+    var user_login_password=document.getElementById('user_login_password');
+    console.log(user_login_password)
+    if (!user_login_id.value) {
+        alert('Please Enter ID!');
+    } 
+    else if (!user_login_password.value){
+        alert('Please Enter Password');
+    }
+    else {
+        user_login_id_value = user_login_id.value;
+        user_login_id.value = "";
+        user_login_password_value = user_login_password.value;
+        user_login_password.value = "";
+        doUserLogin(user_login_id_value,user_login_password_value);
+    }
+};
+
+function doUserLogin(user_id,user_password){
+    var params = {'uID' : user_id,
+                  'uPSWD' : user_password};
+    console.log(params)
+    var body = {};
+    var additionalParams = {};
+
+    
+    apigClient.loginstatusGet(params, body, additionalParams)
+        .then(function(result) {
+            // success callback
+            console.log("Result : ", result);
+            data = result["data"]
+            console.log(data)
+       
+        }).catch(function(result) {
+            // error callback
+            console.log(result);
+        });
+        localStorage.setItem("ID", user_id);
+        window.location.href="index.html";
+}
+
+//要改 晚点改
+function editProfile(){
+    var edit_input_id = document.getElementById('edit_user_id');
+    console.log(edit_input_id)
+    var edit_input_name=document.getElementById('edit_user_name');
+    console.log(edit_input_name)
+    if (!edit_input_id.value) {
+        alert('Please Enter ID!');
+    } else {
+        edit_input_id_value = edit_input_id.value;
+        edit_input_id.value = "";
+        edit_input_name_value = edit_input_name.value;
+        edit_input_name.value = "";
+        // alert(search_input_value);
+        doEditProfile(edit_input_id_value,edit_input_name_value);
+    }
+};
+
+function doEditProfile(edit_id,edit_name){
+    var params = {'uID' : edit_id,
+                  'uFN' :  edit_name};
+    console.log(params)
+    var body = {};
+    var additionalParams = {};
+
+    
+    apigClient.editprofileGet(params, body, additionalParams)
+        .then(function(result) {
+            // success callback
+            console.log("Result : ", result);
+            data = result["data"]
+            console.log(data)
+            
+
+        }).catch(function(result) {
+            // error callback
+            console.log(result);
+        });
+}
+
+function isInt(value) {
+    if (isNaN(value)) {
+      return false;
+    }
+    var x = parseFloat(value);
+    return (x | 0) === x;
+  }
+
+
+function createMeeting(){
+    var create_duration=document.getElementById('create_meeting_duration')
+    console.log(create_duration)
+    var create_host = document.getElementById('create_meeting_host');
+    console.log(create_host)
+    var create_location=document.getElementById('create_meeting_location')
+    console.log(create_location)
+    var create_description = document.getElementById('create_meeting_description');
+    console.log(create_description)
+    var create_participants=document.getElementById('create_meeting_participants');
+    console.log(create_participants)
+
+    if (!create_duration.value) {
+        alert('Please Enter Duration!');
+    } 
+    else if (!isInt(create_duration.value)){
+        console.log(create_duration.value);
+        alert('Duration must be an integer number!');
+    }
+    else if (!create_host.value){
+        alert('Please Enter Host ID');
+    }
+    else if (!create_location.value){
+        alert('Please Enter Host Location');
+    }
+    else if (!create_description.value){
+        alert('Please Enter Description');
+    }
+    else if (!create_participants.value){
+        alert('Please Enter Meeting Participants');
+    }
+    
+    else {
+        create_duration_value = create_duration.value;
+        create_duration.value = "";
+        create_host_value = create_host.value;
+        create_host_value.value = "";
+        create_location_value = create_location.value;
+        create_location.value = "";
+        create_description_value = create_description.value;
+        create_description_value.value = "";
+        create_participants_value = create_participants.value;
+        create_participants.value = "";
+
+        doCreateMeeting(create_duration_value,create_host_value,create_location_value,create_description_value,create_participants_value);
+    }
+};
+
+function doCreateMeeting(c_duration,c_host,c_location,c_description,c_participants){
+    var params={};
+    var bodystr = {
+            "users":c_participants,
+            "duration":parseInt(c_duration),
+            "host":c_host,
+            "location":c_location,
+            "description":c_description
+    };
+    body=JSON.stringify(bodystr)
+    console.log(body)
+    var additionalParams = {};
+
+    
+    apigClient.createmeetingPost(params, body, additionalParams)
+        .then(function(result) {
+            // success callback
+            console.log("Result : ", result);
+            data = result["data"]
+            console.log(data)
+       
+        }).catch(function(result) {
+            // error callback
+            console.log(result);
+        });
+}
+
+function onload_test(){
+    console.log("test onloading");
+}
+
+function findUserMeetings(){
+    var user_current_id =  localStorage.getItem("ID"); //static variable maybe.
+    doFindUserMeetings(user_current_id);
+};
+
+function doFindUserMeetings(cur_id){
+    var params = {'uID' : cur_id};
+    console.log(params)
+    var body = {};
+    var additionalParams = {};
+
+    var res_html = "";
+
+    
+    apigClient.findusermeetingsGet(params, body, additionalParams)
+        .then(function(result) {
+            // success callback
+            // console.log("Result : ", result);
+            var data = result["data"];
+            var idx = 1;
+
+            while(data["Meeting"+idx]!=undefined){
+                console.log(data["Meeting"+idx]);
+
+                var info = data["Meeting"+idx];
+                var meet_date =  new Date();
+                meet_date.setDate(today.getDate() + info["current_set_time"][0]);
+                meet_date = meet_date.toISOString().split("T")[0] + " " + info["current_set_time"][1] + ":00";
+
+                console.log(meet_date);
+
+                res_html += 
+                '<div class="nearby-user">'
+                    +'<div class="row">'
+                      +'<div class="col-md-2 col-sm-2">'
+                          +'<h4>Meeting '+idx+'</h4>'
+                      +'</div>'
+                      +'<div class="col-md-7 col-sm-7">'
+                        +'<h5 class="h5-black">Host: <p class="text-muted p-inline text-blue">'+info["host"]+'</p></h5>'
+                        +'<h5 class="h5-black">Participants: <p class="text-muted p-inline text-blue">'+info["participants"]+'</p></h5>' 
+                        +'<h5 class="h5-black">Current Set Time: <p class="text-muted p-inline text-important">'+meet_date+'</p></h5>'
+                        +'<h5 class="h5-black">Vote Status: <p class="text-muted p-inline text-important">'+info["vote_status"]+'</p></h5>'
+                        +'<h5 class="h5-black">Location: <p class="text-muted p-inline text-blue">'+info["location"]+'</p></h5>'
+                        +'<h5 class="h5-black">Description: <p class="text-muted p-inline text-blue">'+info["description"]+'</p></h5>'
+                        
+                      +'</div>'
+                      +'<div class="col-md-3 col-sm-3">'
+                        +'<a href="meeting-timeslot.html?meeting-idx='+idx+'&host='+info["host"]+'" class="btn btn-box">Choose your available timeslots</a>'
+                      +'</div>'
+                    +'</div>'
+                  +'</div>';
+
+                idx++;
+            }
+            var div = document.getElementById("meeting-info");
+            div.innerHTML = res_html;
+       
+        }).catch(function(result) {
+            // error callback
+            console.log(result);
+        });
+
+}
+
+//sample
+
+function doRecommendTimeSlot(){
+
+    // var pa = window.location.split('?')[1];
+    // console.log(pa);
+
+    var urlquery = window.location.search;
+    var query = urlquery.split("&")
+    var mID = query[0].split("=")[1]
+    var host = query[1].split("=")[1]
+    console.log(mID, host)
+
+    document.getElementById("meeting-title").innerHTML="For meeting "+mID
+    document.getElementById("meeting-title-host").innerHTML="Host: "+host
+
+    var params = {'mID' : mID};
+    console.log(params)
+    var body = {};
+    var additionalParams = {};
+
+    var res_html = '<h4> Recommended timeslots</h4>';
+
+    apigClient.getrecommendGet(params, body, additionalParams)
+    .then(function(result) {
+        // success callback
+        // console.log("Result : ", result);
+        data = result["data"];
+        console.log("recommend data:", data)
+
+        if(data == "{}"){
+            res_html += '<p class="text-important">No available time for you, please inform participants and reset the meeting</p>'
+        }
+        else{
+            res_html += '<div class="people-nearby timeslot-container" id="timeslot-container"><div class="timeslot-content" id="timeslot-content"><form class="form">'
+            timeslots = data.slice(1, -1).split(", ");
+            for(var i=0; i<timeslots.length; i++){
+                day = timeslots[i].split(":")[0].slice(1, -1);
+                day = parseInt(day);
+                time = timeslots[i].split(":")[1].slice(2, -1).split(",");
+                
+                if(time != ''){
+                    for(var t=0; t<time.length; t++){
+                        var meet_date =  new Date();
+                        meet_date.setDate(today.getDate() + day);
+                        meet_date = meet_date.toISOString().split("T")[0] + " " + time[t] + ":00 (next "+day+" day)";
+                        
+                        var label = day + ":" + time[t]
+                        res_html += 
+                            '<div class="inputGroup">'
+                            +'<input class="checkbox-time" id="option'+label+'" value="'+label+'" type="checkbox"/>'
+                            +'<label for="option'+label+'">'+meet_date+'</label>'
+                            +'</div>'
+                        // console.log(meet_date);
+                    }
+                }
+
+            }
+            res_html += '</form></div></div>'
+        }
+
+        var div = document.getElementById("timeslot-space");
+        div.innerHTML = res_html;
+
+   
+    }).catch(function(result) {
+        // error callback
+        console.log(result);
+    });
+}
+
+function voteMeeting(){
+    console.log("vote meeting!");
+    var urlquery = window.location.search;
+    var query = urlquery.split("&");
+    var uID = localStorage.getItem("ID")
+    var mID = query[0].split("=")[1];
+    var day = "";
+    var time = "";
+
+    var checkedTime = document.querySelectorAll('.checkbox-time:checked');
+    for (var i = 0; i < checkedTime.length; i++) {
+        day += checkedTime[i].value.split(":")[0]+",";
+        time += checkedTime[i].value.split(":")[1]+",";
+    }
+    day = day.slice(0, -1);
+    time = time.slice(0, -1);
+
+    // console.log(uID)
+    // console.log(mID)
+    // console.log(day)
+    // console.log(time)
+    console.log(uID, mID, day, time)
+    doVoteMeeting(uID, mID, day, time);
+    window.location.href="meeting.html";
+}
+
+function doVoteMeeting(a,b,c,d){
+    var params={};
+    var bodystr = {
+            "user":a,
+            "meetID":b,
+            "day":c,
+            "time":d
+           
+    };
+    body=JSON.stringify(bodystr)
+    console.log(body)
+    var additionalParams = {};
+
+    apigClient.votemeetingPost(params, body, additionalParams)
+        .then(function(result) {
+            // success callback
+            console.log("Result : ", result);
+            data = result["data"]
+            console.log(data)
+       
+        }).catch(function(result) {
+            // error callback
+            console.log(result);
+        });
+}
